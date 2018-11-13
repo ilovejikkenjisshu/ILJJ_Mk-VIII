42b0c81202fe527eeb3b987b135d66158e3e0046
proelbtn <proelbtn@users.noreply.github.com>
2018-11-13 11:30:47 +0900

    determine the assignment of ports

--- alu-test.ino
+++ alu-test.ino
@@ -1,3 +1,15 @@
+#define F2    32
+#define F1    31
+#define F0    30
+#define Cout  34
+#define Cin   33
+#define S1    41
+#define S0    40
+#define A1    51
+#define A0    50
+#define B1    53
+#define B0    52
+
 void setup() {
   // put your setup code here, to run once:
 
